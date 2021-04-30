2d0f6595fd5998dd5b546d50f484fa57a546ff3f
ketan shah <shahketan1810@gmail.com>
2021-04-30 16:20:58 +0530

    added maps for the campgrounds

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,5 +1,8 @@
 const Campground = require('../models/campground');
 const {cloudinary} = require('../cloudinary');
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const mapBoxToken =  process.env.MAPBOX_TOKEN;
+const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
 
 module.exports.index = async (req, res) => {
     const campgrounds = await Campground.find({});
@@ -11,7 +14,12 @@ module.exports.renderNewForm = (req, res) => {
 }
 
 module.exports.createCampground = async (req, res, next) => {
+    const geoData = await geocoder.forwardGeocode({
+        query: req.body.campground.location,
+        limit: 1
+    }).send()
     const campground = new Campground(req.body.campground);
+    campground.geometry = geoData.body.features[0].geometry;
     campground.images = req.files.map(f => ({ url: f.path, filename: f.filename }));
     campground.author = req.user._id;
     await campground.save();
--- seeds/index.js
+++ seeds/index.js
@@ -1,7 +1,14 @@
+if (process.env.NODE_ENV !== "production"){
+    require('dotenv').config();
+}
+
 const mongoose = require('mongoose');
 const Campground = require('../models/campground');
 const cities = require('./cities');
 const {places,descriptors} = require('./seedHelpers');
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const mapBoxToken =  process.env.MAPBOX_TOKEN;
+const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp',{
     useCreateIndex : true,
@@ -19,23 +26,25 @@ const sample = array=> array[Math.floor(Math.random()*array.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for(let i=0; i<50; i++){
+    for(let i=0; i<20; i++){
         const random1000 = Math.floor(Math.random()*1000);
         const price = Math.floor(Math.random()*20)+10;
+        const location = `${cities[random1000].city}, ${cities[random1000].state}`;
+        const geoData = await geocoder.forwardGeocode({
+            query: location,
+            limit: 1
+        }).send()
+        const geometry = geoData.body.features[0].geometry;
         const camp = new Campground({
             author: '608a1ecf436d1416d03c205c',
-            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            location,
             title: `${sample(descriptors)} ${sample(places)}`,
+            geometry,
             images: [
                 {
-                    url: 'https://res.cloudinary.com/dvmezr2sd/image/upload/v1619708273/YelpCamp/ic3rttux6mayvmq548yt.jpg',
-                    filename: 'YelpCamp/ic3rttux6mayvmq548yt'
-                },
-                {
-                    url: 'https://res.cloudinary.com/dvmezr2sd/image/upload/v1619708273/YelpCamp/pamv0zex7dbz8okgduo2.jpg',
-                    filename: 'YelpCamp/pamv0zex7dbz8okgduo2'
+                    url: 'https://res.cloudinary.com/dvmezr2sd/image/upload/v1619773412/YelpCamp/nyb3cujgqysnlid5vsru.jpg',
+                    filename: 'YelpCamp/nyb3cujgqysnlid5vsru'
                 }
-              
             ],
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
             price
